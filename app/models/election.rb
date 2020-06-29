d0aefc90bf15af794f5b6a8b3c64369d158c7008
osmarluz <osmar.luz@gmail.com>
2020-06-29 20:21:25 -0300

    Add auditable concern to election model

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,14 +1,23 @@
 class Election < ApplicationRecord
-  has_many :questions
+  include Auditable
+
+  has_many :voters, dependent: :destroy
+  has_many :questions, dependent: :destroy
   belongs_to :user
 
   serialize :settings, Hash
 
   def visibility
-    settings[:visibility]
+    settings['visibility']
   end
 
   def visibility=(value)
-    settings[:visibility] = value
+    settings['visibility'] = value
+  end
+
+  def audits
+    Audit.with_auditable(self)
+         .or(Audit.with_entity(self))
+         .descending_order_by_creation_date
   end
 end
--- test/models/election_test.rb
+++ test/models/election_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ElectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  let(:election) { elections(:one) }
+
+  describe 'after create' do
+    it 'creates an audit with new information' do
+      audit = Audit.with_auditable(Election.create user: users(:one)).last
+
+      assert_equal audit.operation, 'create'
+      assert audit.fields.key?('user_id')
+    end
+  end
+
+  describe 'after update' do
+    it 'creates an audit with updated information' do
+      election.update name: 'Updated Name'
+
+      audit = Audit.with_auditable(election).last
+
+      assert_equal audit.operation, 'update'
+      assert audit.fields.key?('name')
+    end
+  end
+
+  describe 'after destroy' do
+    it 'creates an audit with updated information' do
+      election.destroy
+
+      audit = Audit.with_auditable(election).last
+
+      assert_equal audit.operation, 'destroy'
+    end
+  end
 end
