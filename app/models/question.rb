dbc97a7010bf22245a89592f865eb403c965df88
osmarluz <osmar.luz@gmail.com>
2020-06-29 20:21:50 -0300

    Add auditable concern to question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ApplicationRecord
+  include Auditable
+
   belongs_to :election
-  has_many :answers
+  has_many :answers, dependent: :destroy
+
+  after_commit -> { add_association_to_audit(election) }
 end
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  let(:question) { questions(:one) }
+
+  describe 'after create' do
+    it 'creates an audit with new information' do
+      audit = Audit.with_auditable(Question.create election: elections(:one)).last
+
+      assert_equal audit.operation, 'create'
+      assert audit.fields.key?('election_id')
+    end
+  end
+
+  describe 'after update' do
+    it 'creates an audit with updated information' do
+      question.update name: 'Updated Name'
+
+      audit = Audit.with_auditable(question).last
+
+      assert_equal audit.operation, 'update'
+      assert audit.fields.key?('name')
+    end
+  end
+
+  describe 'after destroy' do
+    it 'creates an audit with updated information' do
+      question.destroy
+
+      audit = Audit.with_auditable(question).last
+
+      assert_equal audit.operation, 'destroy'
+    end
+  end
+
+  describe 'after commit' do
+    it 'updates the created audit with election relation' do
+      question.save
+
+      audit = Audit.with_auditable(question).last
+
+      assert_equal audit.entity, question.election
+    end
+  end
 end
