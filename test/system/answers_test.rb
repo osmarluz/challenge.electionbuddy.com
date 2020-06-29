9c47c13c7c59f7689120a76ead44dc2deecabc5e
osmarluz <osmar.luz@gmail.com>
2020-06-29 20:19:16 -0300

    Fix answer's system tests

--- test/system/answers_test.rb
+++ test/system/answers_test.rb
@@ -2,38 +2,112 @@ require "application_system_test_case"
 
 class AnswersTest < ApplicationSystemTestCase
   setup do
-    @answer = answers(:one)
+    @user = users(:one)
+    @question = questions(:one)
   end
 
   test "visiting the index" do
-    visit answers_url
+    visit root_url
+    click_on "Sign In"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: 'password'
+    click_on "Log in"
+    click_on "Show", match: :first
+
+    click_on "Questions"
+
+    click_on "Show", match: :first
+
+    click_on "Answers"
+
     assert_selector "h1", text: "Answers"
+
+    assert_text @question.answers.first.name
   end
 
-  test "creating a Answer" do
-    visit answers_url
+  test "showing an Answer" do
+    visit root_url
+    click_on "Sign In"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: 'password'
+    click_on "Log in"
+    click_on "Show", match: :first
+
+    click_on "Questions"
+
+    click_on "Show", match: :first
+
+    click_on "Answers"
+
+    click_on "Show", match: :first
+
+    assert_text @question.answers.first.name
+
+    click_on "Back"
+  end
+
+  test "creating an Answer" do
+    visit root_url
+    click_on "Sign In"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: 'password'
+    click_on "Log in"
+    click_on "Show", match: :first
+
+    click_on "Questions"
+
+    click_on "Show", match: :first
+
+    click_on "Answers"
+
     click_on "New Answer"
 
-    fill_in "Name", with: @answer.name
+    fill_in "Name", with: 'Answer Create'
+
     click_on "Create Answer"
 
     assert_text "Answer was successfully created"
     click_on "Back"
   end
 
-  test "updating a Answer" do
-    visit answers_url
+  test "updating an Answer" do
+    visit root_url
+    click_on "Sign In"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: 'password'
+    click_on "Log in"
+    click_on "Show", match: :first
+
+    click_on "Questions"
+
+    click_on "Show", match: :first
+
+    click_on "Answers"
+
     click_on "Edit", match: :first
 
-    fill_in "Name", with: @answer.name
+    fill_in "Name", with: 'Answer Update'
+
     click_on "Update Answer"
 
     assert_text "Answer was successfully updated"
     click_on "Back"
   end
 
-  test "destroying a Answer" do
-    visit answers_url
+  test "destroying an Answer" do
+    visit root_url
+    click_on "Sign In"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: 'password'
+    click_on "Log in"
+    click_on "Show", match: :first
+
+    click_on "Questions"
+
+    click_on "Show", match: :first
+
+    click_on "Answers"
+
     page.accept_confirm do
       click_on "Destroy", match: :first
     end
