504270c3b957e018e271fae81095151b040ac274
osmarluz <osmar.luz@gmail.com>
2020-06-29 20:21:36 -0300

    Add auditable concern to voter model

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,3 +1,7 @@
 class Voter < ApplicationRecord
+  include Auditable
+
   belongs_to :election
+
+  after_commit -> { add_association_to_audit(election) }
 end
--- test/models/voter_test.rb
+++ test/models/voter_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class VoterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  let(:voter) { voters(:one) }
+
+  describe 'after create' do
+    it 'creates an audit with new information' do
+      audit = Audit.with_auditable(Voter.create election: elections(:one)).last
+
+      assert_equal audit.operation, 'create'
+      assert audit.fields.key?('election_id')
+    end
+  end
+
+  describe 'after update' do
+    it 'creates an audit with updated information' do
+      voter.update name: 'Updated Name'
+
+      audit = Audit.with_auditable(voter).last
+
+      assert_equal audit.operation, 'update'
+      assert audit.fields.key?('name')
+    end
+  end
+
+  describe 'after destroy' do
+    it 'creates an audit with updated information' do
+      voter.destroy
+
+      audit = Audit.with_auditable(voter).last
+
+      assert_equal audit.operation, 'destroy'
+    end
+  end
+
+  describe 'after commit' do
+    it 'updates the created audit with election relation' do
+      voter.save
+
+      audit = Audit.with_auditable(voter).last
+
+      assert_equal audit.entity, voter.election
+    end
+  end
 end
