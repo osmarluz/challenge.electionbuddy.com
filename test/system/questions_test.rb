b9fd02028009d8a229dff7e66beaf9e746b06b55
osmarluz <osmar.luz@gmail.com>
2020-06-29 20:19:05 -0300

    Fix questions' system tests

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -2,20 +2,56 @@ require "application_system_test_case"
 
 class QuestionsTest < ApplicationSystemTestCase
   setup do
-    @question = questions(:one)
+    @user = users(:one)
+    @election = elections(:one)
   end
 
   test "visiting the index" do
-    visit question_url
+    visit root_url
+    click_on "Sign In"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: 'password'
+    click_on "Log in"
+    click_on "Show", match: :first
+
+    click_on "Questions"
+
     assert_selector "h1", text: "Questions"
+
+    assert_text @election.questions.first.name
+  end
+
+  test "showing a Question" do
+    visit root_url
+    click_on "Sign In"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: 'password'
+    click_on "Log in"
+    click_on "Show", match: :first
+
+    click_on "Questions"
+
+    click_on "Show", match: :first
+
+    assert_text @election.questions.first.name
+
+    click_on "Back"
   end
 
   test "creating a Question" do
-    visit question_url
+    visit root_url
+    click_on "Sign In"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: 'password'
+    click_on "Log in"
+    click_on "Show", match: :first
+
+    click_on "Questions"
+
     click_on "New Question"
 
-    fill_in "Answers", with: @question.answer_ids
-    fill_in "Name", with: @question.name
+    fill_in "Name", with: 'Create Question'
+
     click_on "Create Question"
 
     assert_text "Question was successfully created"
@@ -23,11 +59,19 @@ class QuestionsTest < ApplicationSystemTestCase
   end
 
   test "updating a Question" do
-    visit question_url
+    visit root_url
+    click_on "Sign In"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: 'password'
+    click_on "Log in"
+    click_on "Show", match: :first
+
+    click_on "Questions"
+
     click_on "Edit", match: :first
 
-    fill_in "Answers", with: @question.answer_ids
-    fill_in "Name", with: @question.name
+    fill_in "Name", with: 'Update Question'
+
     click_on "Update Question"
 
     assert_text "Question was successfully updated"
@@ -35,7 +79,15 @@ class QuestionsTest < ApplicationSystemTestCase
   end
 
   test "destroying a Question" do
-    visit question_url
+    visit root_url
+    click_on "Sign In"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: 'password'
+    click_on "Log in"
+    click_on "Show", match: :first
+
+    click_on "Questions"
+
     page.accept_confirm do
       click_on "Destroy", match: :first
     end
